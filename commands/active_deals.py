f54d83f459cd07a755bad77637644e09ecc30618
Batya02 <magomedov.abdul20012@gmail.com>
2021-07-18 23:03:44 +0300

    changes

--- commands/active_deals.py
+++ commands/active_deals.py
@@ -1,6 +1,8 @@
 from objects.globals import dp
 from objects import globals
 
+from db_models.Shops_and_Sales import SAS
+
 from aiogram.types import (
         Message, InlineKeyboardMarkup, 
         InlineKeyboardButton
@@ -8,12 +10,16 @@ from aiogram.types import (
 
 @dp.message_handler(lambda message: message.text == "📁Активные сделки")
 async def activate_deals(message: Message):
+
+    all_shops = await SAS.objects.filter(main_user=message.from_user.id, ended=False).all()
+    all_sales = await SAS.objects.filter(not_main_user=message.from_user.id, ended=False).all()
+
     globals.state_type = "" #Reset state type
     
     buttons = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="Покупки", callback_data="active_shops"), 
-             InlineKeyboardButton(text="Продажи", callback_data="active_sales")]
+            [InlineKeyboardButton(text=f"Покупки({len(all_shops)})", callback_data="active_shops"), 
+             InlineKeyboardButton(text=f"Продажи({len(all_sales)})", callback_data="active_sales")]
         ]
     )
 
